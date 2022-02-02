776961ad989a2701577ff2b78db4f1b073e2e5b7
Asmaa M. Khalaf <asmaa.m.khalaf.fci@gmail.com>
2022-02-02 11:36:25 +0200

    Complete sections styles

--- js/app.js
+++ js/app.js
@@ -1,18 +1,30 @@
+/* global variables */
 const brandLink = document.getElementById("brand-link");
 const hamburgerIcon = document.getElementById("hamburger-icon");
 const closeBtn = document.getElementById("close-btn");
 const nav = document.getElementsByTagName("nav")[0];
 
+
+/* hide the navbar when clicking the hamburger btn */
 hamburgerIcon.addEventListener("click", () => {
   hamburgerIcon.classList.add("hidden");
   brandLink.classList.add("hidden");
   nav.classList.remove("blured-background");
 });
 
+/* show the navbar when clicking the close btn */
 closeBtn.addEventListener("click", () => {
   setTimeout(() => {
     hamburgerIcon.classList.remove("hidden");
     brandLink.classList.remove("hidden");
     nav.classList.add("blured-background");
   }, 250);
+});
+
+
+/* close the navbar when clicking a link */
+nav.addEventListener("click", (e) => {
+  if (e.target.nodeName === "A") {
+    closeBtn.click();
+  }
 });
